ac72c226fd9ae906b7a3900769d53d505c648131
Rob Hamilton <RobHamilton3rd@gmail.com>
2019-11-17 20:14:15 -0500

    added string length

--- PicMetric/models.py
+++ PicMetric/models.py
@@ -4,6 +4,6 @@ DB = SQLAlchemy()
 
 class HashTable(DB.Model):
     id = DB.Column(DB.Integer, primary_key=True)
-    hash = DB.Column(DB.String(20), nullable=False)
+    hash = DB.Column(DB.String(50), nullable=False)
     pred = DB.Column(DB.String(100), nullable=False)
     url = DB.Column(DB.String(100), nullable=False)
