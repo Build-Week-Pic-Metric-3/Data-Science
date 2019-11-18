27209214b4fdfa9bb4d8d854f3b621fbb4948716
distortedlogic <jermeek@users.noreply.github.com>
2019-11-18 17:04:12 -0500

    only one url

--- PicMetric/routes/do_data_science.py
+++ PicMetric/routes/do_data_science.py
@@ -10,11 +10,7 @@ do_data_science_bp = Blueprint('do_data_science_bp', __name__)
 @do_data_science_bp.route('/do_data_science', methods=['GET', 'POST'])
 def do_data_science():
     url_list = [
-        'https://i.redd.it/t5gqutndubs31.png',
-        'https://i.redd.it/wutifutmubs31.jpg',
-        'https://i.redd.it/5q7spoypubs31.jpg',
-        'https://i.redd.it/a2koy4ptubs31.jpg',
-        'https://i.redd.it/pk77o0u4vbs31.jpg'
+        'https://i.redd.it/t5gqutndubs31.png'
     ]
     model_list = [resnet]
 
