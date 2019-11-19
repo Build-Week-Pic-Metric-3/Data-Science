6594f1cbcca59899bef55d0642a5b5c2cf085d02
distortedlogic <jermeek@users.noreply.github.com>
2019-11-19 16:44:07 -0500

    DB added back

--- PicMetric/app.py
+++ PicMetric/app.py
@@ -1,9 +1,7 @@
 from flask import Flask, redirect, url_for
 
-
-from .routes.summary import sum_bp
-from .routes.batch_img_summary import bis_bp
-from .models import DB
+from PicMetric.routes.do_data_science import do_data_science_bp
+from .schema import DB
 
 from decouple import config
 from dotenv import load_dotenv
@@ -13,6 +11,11 @@ load_dotenv()
 
 def create_app():
     app = Flask(__name__)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    DB.init_app(app)
     
 
     app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
@@ -30,5 +33,6 @@ def create_app():
     def reset():
         DB.drop_all()
         DB.create_all()
-        return redirect(url_for('sum_bp.summary'))
+        return 'DataBase Reset Successful'
+
     return app
--- PicMetric/classes/img_handler.py
+++ PicMetric/classes/img_handler.py
@@ -1,5 +1,12 @@
-from PicMetric.functions.img import get_pred_data
-import json
+import io
+import os
+import hashlib
+import requests
+
+from PIL import Image
+from PicMetric.schema import DB, HashTable
+
+IMGDIR_PATH = 'PicMetric/assets'
 
 class Img_Handler:
     def __init__(self, url_list):
@@ -10,7 +17,52 @@ class Img_Handler:
         for url in self.url_list:
             data[url] = get_pred_data(url, model_func)
         return data
-        
 
+    def get_pred_data(self, url):
+        data = {'source': url}
+
+        is_url_dup = HashTable.query.filter(HashTable.source == url).all()
+        if is_url_dup:
+            for model in self.model_list:
+                data[model.__name__] = getattr(is_url_dup[0], model.__name__)
+            data['hash'] = is_url_dup[0].hash
+            data['error'] = ""
+            return data
+
+        try:
+            with requests.get(url) as response:
+                content = response.content
+                raw = io.BytesIO(response.content)
+
+        except:
+            data['error'] = 'url_error'
+
+            return data
+        
+        else:
+            data['hash'] = hashlib.md5(content).hexdigest()
+            is_img_dup = HashTable.query.filter(HashTable.hash == data['hash']).all()
+            if is_img_dup:
+                for model in self.model_list:
+                    data[model.__name__] = getattr(is_img_dup[0], model.__name__)
+                data['hash'] = is_img_dup[0].hash
+                data['error'] = ""
+                return data
+            
+            output_filename = os.path.join(IMGDIR_PATH, f"{data['hash']}.png")
+            img = Image.open(raw)
+
+            with open(output_filename, 'wb') as out_file:
+                img.save(out_file, format='png')
+
+            for model in self.model_list:
+                data[model.__name__] = str(model(output_filename))
+
+            db_entry = HashTable(**data)
+            DB.session.add(db_entry)
+            DB.session.commit()
+
+            os.remove(output_filename)
+            data['error'] = ""
 
         
