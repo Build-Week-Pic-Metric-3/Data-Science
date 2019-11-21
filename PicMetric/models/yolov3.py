25fe1a5b9d24e1896c4107e1d4d2e1479228e997
distortedlogic <jermeek@users.noreply.github.com>
2019-11-20 21:14:33 -0500

    png fix

--- PicMetric/models/yolov3.py
+++ PicMetric/models/yolov3.py
@@ -9,7 +9,7 @@ from PicMetric.functions.s3_funcs import upload_file_to_s3, S3_BUCKET, s3, S3_LO
 
 weights_url = 'https://github.com/OlafenwaMoses/ImageAI/releases/download/1.0/yolo.h5'
 weights_path = "PicMetric/assets/weights/yolo.h5"
-temp_output_path = os.path.join('PicMetric/assets/temp', "temp.jpg")
+temp_output_path = os.path.join('PicMetric/assets/temp', "temp.png")
 
 
 
@@ -28,7 +28,7 @@ def yolov3(input_path):
     detection = detector.detectObjectsFromImage(input_image=input_path, output_image_path=temp_output_path)
 
     data = dict()
-    filename= os.path.splitext(os.path.basename(input_path))[0] + '_yolov3.jpg'
+    filename= os.path.splitext(os.path.basename(input_path))[0] + '_yolov3.png'
     data['url'] = "{}{}".format(S3_LOCATION, filename)
     s3.upload_file(temp_output_path, S3_BUCKET, filename)
     
