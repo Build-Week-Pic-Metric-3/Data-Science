8c2107f7985ba5ea15303ac6060b559ba4c677fd
Rob Hamilton <RobHamilton3rd@gmail.com>
2019-11-22 11:11:18 -0500

    updated ubuntu bootstrap flow

--- ubuntu_bootstrap.sh
+++ ubuntu_bootstrap.sh
@@ -1,5 +1,10 @@
 sudo apt install gunicorn -y
+sudo apt install gcc -y
 sudo apt install python3-flask -y
+sudo apt install virtualenv -y
+sudo apt-get install libpq-dev python-dev -y
+sudo apt install postgresql -y
+sudo apt update && sudo apt install -y libsm6 libxext6 -y
 mkdir PicMetric
 cd PicMetric
 virtualenv -p python3 picmetricenv
@@ -9,6 +14,6 @@ mkdir PicMetric/assets/weights
 cd PicMetric/assets/weights/
 wget https://github.com/OlafenwaMoses/ImageAI/releases/download/1.0/yolo.h5
 echo "Activate your virtual enviornment and run"
-echo "source picmetric/bin/activate"
+echo "source picmetricenv/bin/activate"
 echo "pip install -r ubuntu_requirements.txt"
 echo "Also update .env file one level above the flask app!!!"
