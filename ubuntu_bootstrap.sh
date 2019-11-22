77293f2e2c1b3bd015091e1eb1def49516555dcb
Rob Hamilton <RobHamilton3rd@gmail.com>
2019-11-22 10:06:38 -0500

    updated bootstrap

--- ubuntu_bootstrap.sh
+++ ubuntu_bootstrap.sh
@@ -1,9 +1,14 @@
 sudo apt install gunicorn -y
-mkdir picmetric
-python3 -m venv env/
-source env/bin/activate
+sudo apt install python3-flask -y
+mkdir PicMetric
+cd PicMetric
+virtualenv -p python3 picmetricenv
 git init
 git pull https://github.com/Build-Week-Pic-Metric-3/Data-Science.git master
 mkdir PicMetric/assets/weights
 cd PicMetric/assets/weights/
-pip install flask requests python-decouple boto3 pillow flask-sqlalchemy numpy tensorflow imageai opencv-python keras mtcnn flask-cors python-dotenv1
+wget https://github.com/OlafenwaMoses/ImageAI/releases/download/1.0/yolo.h5
+echo "Activate your virtual enviornment and run"
+echo "source picmetric/bin/activate"
+echo "pip install -r ubuntu_requirements.txt"
+echo "Also update .env file one level above the flask app!!!"
